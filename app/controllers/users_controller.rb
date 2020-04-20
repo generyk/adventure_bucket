20351a574b036f62c60d3e05d4d73c1d77e4fb3f
Eryk Rachwal <erykrachwal@yahoo.com>
2020-04-20 05:05:54 +0000

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   
+  get '/signup' do 
+    erb :'users/signup'
+  end 
   
+  post '/users' do 
+    user = User.create(params)
+    redirect to "/users/#{user.id}"
+  end 
   
+  get '/users/:id' do 
+    @user = User.find_by(id: params[:id])
+    erb :'users/show'
+  end 
 end 
