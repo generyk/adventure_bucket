c19a0d50d0e0f97c9c42c53780dea397704858a1
Eryk Rachwal <erykrachwal@yahoo.com>
2020-04-20 05:50:21 +0000

    sign-up, login, validations, user-show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,36 @@ class UsersController < ApplicationController
     erb :'users/signup'
   end 
   
-  post '/users' do 
+  get '/login' do 
+    erb :'users/login'
+  end 
+  
+  post '/login' do 
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect to "/users/#{user.id}"
+    else 
+      redirect to '/signup'
+    end 
+  end 
+
+  post '/signup' do 
     user = User.create(params)
-    redirect to "/users/#{user.id}"
+    if user.valid?
+      session[:user_id] = user.id 
+      redirect to "/users/#{user.id}"
+    else 
+      redirect to '/signup'
+    end 
   end 
   
   get '/users/:id' do 
+    if User.find_by(id: params[:id])
     @user = User.find_by(id: params[:id])
+    else 
+      redirect to '/'
+    end 
     erb :'users/show'
   end 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base 
   has_many :destinations 
   
+  validates :username, presence: true, uniqueness: true 
+  validates :email, presence: true, uniqueness: true
+  
   has_secure_password 
   
 end 
+
